ed671cafa182f835fdbc215518f985d26af38f07
facundo174 <113550421+facundo174@users.noreply.github.com>
2022-12-31 13:08:51 -0300

    Update cuentas.sql

--- cuentas.sql
+++ cuentas.sql
@@ -1,3 +1,7 @@
+
+
+CREATE database cuentas;
+
 CREATE TABLE users(
 id INT unsigned AUTO_INCREMENT PRIMARY KEY ,
 username VARCHAR(20) NOT NULL,
@@ -11,13 +15,5 @@ signup_date datetime NOT NULL
 
 
 
-DROP table users; 
-
-DESCRIBE users;
-DELETE from users;
-use homebanking;
 
 
-SELECT * FROM users;
-INSERT INTO users(username, password, name, last_name, email, signup_date)
- values ("admin", "admin123", "Pepe", "Grillo", "pepegrillo@gmail.com",  '2022-07-08 15:30:00');
